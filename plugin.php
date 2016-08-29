bdae2ca613bd6e3d486c7f921c6561b5fc3114b0
mgibbs189 <mgibbs189@gmail.com>
2016-08-29 13:38:07 -0400

    Config tweaks, supported GitHub Updater

--- plugin.php
+++ plugin.php
@@ -1,23 +1,20 @@
 <?php
-/**
- * Plugin Name: Facet Stack for FacetWP
- * Plugin URI:  http://cramer.co.za
- * Description: A Widget for creating a stack of Facets for FacetWP
- * Version:     1.0.0
- * Author:      David Cramer
- * Author URI:  http://cramer.co.za
- * License:     GPL-2.0+
- * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
- */
+/*
+Plugin Name: FacetWP - Facet Stack
+Description: A widget for creating a stack of facets
+Version: 1.0
+Author: David Cramer
+Author URI: http://cramer.co.za
+GitHub URI: Desertsnowman/facet-stack
+License: GPLv2 or later
+*/
 
-if ( ! defined( 'WPINC' ) ) { // ye, thats not going to happen, sorry.
-	die;
-}
+defined( 'ABSPATH' ) or exit;
 
 // setup constants
 define( 'FACET_STACK_PATH', plugin_dir_path( __FILE__ ) );
 define( 'FACET_STACK_URL', plugin_dir_url( __FILE__ ) );
-define( 'FACET_STACK_VER', '1.0.0' );
+define( 'FACET_STACK_VER', '1.0' );
 define( 'FACET_STACK_BASENAME', plugin_basename( __FILE__ ) );
 
 // load widget and initilize Facet Stack.
