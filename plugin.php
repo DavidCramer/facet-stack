4e0435c81b8f2710f6958a912f77d9e549c7af84
mgibbs189 <mgibbs189@gmail.com>
2017-07-27 10:01:31 -0400

    Tagged 1.1

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: FacetWP - Facet Stack
 Description: A widget for creating a stack of facets
-Version: 1.0
+Version: 1.1
 Author: David Cramer
 Author URI: http://cramer.co.za
 GitHub URI: Desertsnowman/facet-stack
@@ -14,7 +14,7 @@ defined( 'ABSPATH' ) or exit;
 // setup constants
 define( 'FACET_STACK_PATH', plugin_dir_path( __FILE__ ) );
 define( 'FACET_STACK_URL', plugin_dir_url( __FILE__ ) );
-define( 'FACET_STACK_VER', '1.0' );
+define( 'FACET_STACK_VER', '1.1' );
 define( 'FACET_STACK_BASENAME', plugin_basename( __FILE__ ) );
 
 // load widget and initilize Facet Stack.
